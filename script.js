78c2c769fc297012c1a823bbddc06147d242e28d
Carolescola <145365054+Carolescola@users.noreply.github.com>
2025-08-21 07:51:03 -0300

    .

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "A psicquIA, é uma junção de tecnologia e psicologia, nos ultimos dias, muito se fala em desabafar de uma forma “inovadora” para muitos isso parece legal, mas afinal, será que isso é realmente seguro?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Isso me assusta!",
+                afirmacao: "Isso é perfeito! "
             },
             {
                 texto: "Isso é maravilhoso!",
